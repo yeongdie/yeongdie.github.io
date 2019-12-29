d78e93699fdfae9744e1e5bdaaf74c3bdf82b564
sungchuni <new.sungchuni@gmail.com>
2019-12-29 21:20:31 +0900

    add wwwPath to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,12 @@ const webpack = require("webpack");
 
 const isProduction = process.env.NODE_ENV === "production";
 const serverPath = process.cwd();
+const wwwPath = /Windows/.test(process.env.OS)
+  ? path.resolve("/", "inetpub", "wwwroot")
+  : path.resolve("/", "Users", "jenny", "www");
 const distPath = isProduction
   ? path.resolve(serverPath, "asset")
-  : path.resolve("/Users", "jenny", "www", "yeongdie", "asset");
+  : path.resolve(wwwPath, "yeongdie", "asset");
 const srcPath = path.resolve(serverPath, "src");
 
 module.exports = {
