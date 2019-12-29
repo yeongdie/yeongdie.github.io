e0d75affc6395e5598893711d371012952081d08
sungchuni <new.sungchuni@gmail.com>
2019-12-29 23:06:02 +0900

    add popstate event handler

--- src/Main.js
+++ src/Main.js
@@ -19,11 +19,29 @@ export default class Main extends React.Component {
       cat: true
     };
     this.catClick = this.catClick.bind(this);
+    this.catPopstate = this.catPopstate.bind(this);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("popstate", this.catPopstate);
   }
   catClick() {
+    window.history.pushState(this.state, "", "?cat=hide");
+    window.addEventListener("popstate", this.catPopstate);
     this.setState({
       cat: false
     });
+  }
+  catPopstate() {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.get("cat") === "hide") {
+      this.setState({
+        cat: false
+      });
+    } else {
+      this.setState({
+        cat: true
+      });
+    }
   }
   render() {
     return (
